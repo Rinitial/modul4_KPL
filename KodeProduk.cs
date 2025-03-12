e4c6b802ea188b83cfeb8f01590947cde4f2d253
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-12 15:55:50 +0700

    Menambahkan KodeProduk

--- KodeProduk.cs
+++ KodeProduk.cs
@@ -1,8 +1,21 @@
 ﻿using System;
+using System.Reflection;
 
 public class KodeProduk
 {
-	public KodeProduk()
+	public static String[] produk = { Laptop, Smartphone, Tablet, Headset, Keyboard, Mouse, Printer, Monitor, Smartwatch, Kamera };
+
+	public static String[] kodeProduk = { E100, E101, E102, E103, E104, E105, E106, E107, E108, E109 };
+
+	public static int GetKodeProduk(String namaProduk)
 	{
+		for (int i = 0; i < produk.Length; i++)
+		{
+			if (produk[i] == namaProduk)
+			{
+				return KodeProduk[i];
+			}
+		}
+		throw new ArgumentException("Produk Tidak Ditemuakan");
 	}
 }
